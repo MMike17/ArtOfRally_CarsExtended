be27e558cbe63b7b674d7cdd6865b340ad9c5956
MMike17 <matthews.r.mike@gmail.com>
2025-03-13 19:41:33 +0100

    Add structure classes for main features

---
+++ CarSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace CarsExtended
+{
+    internal static class CarSpawner
+    {
+        // TODO : Check that the car mode spawning works when we try to select a car (might have to spawn cars there)
+
+        // where do we spawn cars ?
+
+        // GhostManager.(public void InitializeGhost())
+        // PlayerManager.(public GameObject CreateCar(bool isForwardStage, string carPrefabName, Transform optionalSpawnPosition))
+
+        // TODO : What infos do I need to lift from the og ?
+        public static void SpawnCar(Transform parent, Vector3 position, Quaternion rotation, bool isLocalPos, bool isLocalRot)
+        {
+            //
+        }
+    }
+}
---
+++ Patches/GhostManager_Patch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+
+namespace CarsExtended
+{
+    [HarmonyPatch(typeof(GhostManager), nameof(GhostManager.InitializeGhost))]
+    internal static class GhostManager_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            // cancel Resources.Load call and replace with CarSpawner.SpawnCar()
+        }
+    }
+}
