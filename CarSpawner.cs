8dd1aa2e7696657a69d67e638b958a9b8d2bf12f
MMike17 <matthews.r.mike@gmail.com>
2025-04-21 23:12:23 +0200

    Refactor car configuration

--- CarSpawner.cs
+++ CarSpawner.cs
@@ -24,56 +24,34 @@ namespace CarsExtended
 
             // TODO : Check if I have any race conditions
 
-            SetupRigidbody(carObj.GetComponent<Rigidbody>() ?? carObj.AddComponent<Rigidbody>());
-            SetupDrivetrain(carObj.AddComponent<Drivetrain>(), carInfos);
-            SetupCarDynamics(carObj.AddComponent<CarDynamics>());
-            carObj.AddComponent<Arcader>();
+            Transform lightsTransform = carObj.transform.Find("PrefabSpawns/Lights");
+            Transform brakelightsTransform = lightsTransform.Find("Brakelights");
+            GameObject sourceCarPrefab = Resources.Load<GameObject>(
+                "Prefabs/Cars/" + carInfos.soundSourcePrefabName + "/" + carInfos.soundSourcePrefabName
+            );
+
             carObj.AddComponent<AerodynamicResistance>();
-            Axles axles = SetupAxles(carObj.AddComponent<Axles>(), carInfos);
+            carObj.AddComponent<Arcader>();
             carObj.AddComponent<AxisCarController>();
-            SetupWings(carObj);
-            SetupSoundController(carObj.AddComponent<SoundController>(), carInfos);
-            carObj.AddComponent<SkidmarksManager>();
             carObj.AddComponent<PlayerCollider>();
             carObj.AddComponent<ResetCar>();
-            SetupPlayerVibrator(carObj.AddComponent<PlayerVibrator>(), axles);
+            carObj.AddComponent<SkidmarksManager>();
+
+            Axles axles = SetupAxles(carObj.AddComponent<Axles>(), carInfos);
+            SetupCarDynamics(carObj.AddComponent<CarDynamics>(), brakelightsTransform);
             SetupCollider(carObj);
-            SetupParticleManager(carObj);
-            SetupHeadlights(carObj);
-            SetupShadow(carObj, carInfos);
+            SetupDrivetrain(carObj.AddComponent<Drivetrain>(), carInfos);
+            SetupHeadlights(carObj, lightsTransform);
+            SetupParticleManager(carObj, lightsTransform, brakelightsTransform);
+            SetupPlayerVibrator(carObj.AddComponent<PlayerVibrator>(), axles);
             SetupRenderers(carObj, carInfos);
+            SetupRigidbody(carObj.GetComponent<Rigidbody>() ?? carObj.AddComponent<Rigidbody>());
+            SetupShadow(carObj, sourceCarPrefab);
+            SetupSoundController(carObj.AddComponent<SoundController>(), sourceCarPrefab.GetComponent<SoundController>());
+            SetupWings(carObj);
 
+            Resources.UnloadAsset(sourceCarPrefab);
             carObj.SetActive(true);
-        }
-
-        // force setup rigidbody because I don't trust humans
-        private static void SetupRigidbody(Rigidbody rigidbody)
-        {
-            rigidbody.drag = 0;
-            rigidbody.angularDrag = 0;
-            rigidbody.useGravity = true;
-            rigidbody.isKinematic = false;
-            rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
-            rigidbody.collisionDetectionMode = CollisionDetectionMode.Discrete;
-            rigidbody.constraints = RigidbodyConstraints.None;
-        }
-
-        private static void SetupDrivetrain(Drivetrain drivetrain, CarInfos carInfos)
-        {
-            drivetrain.transmissionForParticles = (TransmissionForParticles)Enum.Parse(
-                typeof(TransmissionForParticles),
-                carInfos.data.carStats.Transmission.ToString()
-            );
-            drivetrain.engineOrientation = carInfos.engineOrientation;
-        }
-
-        private static void SetupCarDynamics(CarDynamics carDynamics)
-        {
-            Transform brakeTransform = carDynamics.transform.Find("PrefabSpawns/Lights/Brakelights");
-            BrakeEffects brakeEffects = brakeTransform.gameObject.AddComponent<BrakeEffects>();
-
-            brakeEffects.RightBrakeLightTransform = brakeTransform.Find("BreakLight R");
-            brakeEffects.LeftBrakeLightTransform = brakeTransform.Find("BreakLight L");
         }
 
         private static Axles SetupAxles(Axles axles, CarInfos carInfos)
@@ -115,70 +93,12 @@ namespace CarsExtended
             return wheel;
         }
 
-        private static void SetupWings(GameObject carObj)
+        private static void SetupCarDynamics(CarDynamics carDynamics, Transform brakelightsTransform)
         {
-            carObj.transform.Find("Wing_Front").gameObject.AddComponent<Wing>().WingPosition = Wing.WING_POSITION.FRONT;
-            carObj.transform.Find("Wing_Back").gameObject.AddComponent<Wing>().WingPosition = Wing.WING_POSITION.FRONT;
-        }
+            BrakeEffects brakeEffects = brakelightsTransform.gameObject.AddComponent<BrakeEffects>();
 
-        private static void SetupSoundController(SoundController soundController, CarInfos carInfos)
-        {
-            GameObject sourcePrefab = Resources.Load<GameObject>(
-                "Prefabs/Cars/" + carInfos.soundSourcePrefabName + "/" + carInfos.soundSourcePrefabName
-            );
-            SoundController source = sourcePrefab.GetComponent<SoundController>();
-
-            soundController.antiLagEnabled = source.antiLagEnabled;
-            soundController.playExhaustParticles = source.playExhaustParticles;
-            soundController.flatTireVolume = source.flatTireVolume;
-            soundController.flatTire = source.flatTire;
-            soundController.waterSplashNoise = source.waterSplashNoise;
-            soundController.rollingNoiseOffroad = source.rollingNoiseOffroad;
-            soundController.rollingNoiseSand = source.rollingNoiseSand;
-            soundController.rollingNoiseGrass = source.rollingNoiseGrass;
-            soundController.windVolume = source.windVolume;
-            soundController.wind = source.wind;
-            soundController.antilag1 = source.antilag1;
-            soundController.backfire3 = source.backfire3;
-            soundController.backfire2 = source.backfire2;
-            soundController.backfire1 = source.backfire1;
-            soundController.turboWasteGate3 = source.turboWasteGate3;
-            soundController.turboWasteGate2 = source.turboWasteGate2;
-            soundController.turboWasteGate1 = source.turboWasteGate1;
-            soundController.shiftTriggerVolume = source.shiftTriggerVolume;
-            soundController.shiftTrigger = source.shiftTrigger;
-            soundController.scrapeNoiseVolume = source.scrapeNoiseVolume;
-            soundController.scrapeNoise = source.scrapeNoise;
-            soundController.crashLowVolume = source.crashLowVolume;
-            soundController.crashLowSpeed = source.crashLowSpeed;
-            soundController.crashHighVolume = source.crashHighVolume;
-            soundController.crashHiSpeed = source.crashHiSpeed;
-            soundController.skidPitchFactor = source.skidPitchFactor;
-            soundController.gravelSkid = source.gravelSkid;
-            soundController.tarmacWetSkid = source.tarmacWetSkid;
-            soundController.tarmacDrySkid = source.tarmacDrySkid;
-            soundController.brakeNoiseVolume = source.brakeNoiseVolume;
-            soundController.brakeNoise = source.brakeNoise;
-            soundController.transmissionSourcePitch = source.transmissionSourcePitch;
-            soundController.transmissionVolumeReverse = source.transmissionVolumeReverse;
-            soundController.transmissionVolume = source.transmissionVolume;
-            soundController.transmission = source.transmission;
-            soundController.startEnginePitch = source.startEnginePitch;
-            soundController.startEngineVolume = source.startEngineVolume;
-            soundController.startEngine = source.startEngine;
-            soundController.engineNoThrottlePitchFactor = source.engineNoThrottlePitchFactor;
-            soundController.engineNoThrottleVolume = source.engineNoThrottleVolume;
-            soundController.engineNoThrottle = source.engineNoThrottle;
-            soundController.engineThrottlePitchFactor = source.engineThrottlePitchFactor;
-            soundController.engineThrottleVolume = source.engineThrottleVolume;
-            soundController.engineThrottle = source.engineThrottle;
-
-            Resources.UnloadAsset(sourcePrefab);
-        }
-
-        private static void SetupPlayerVibrator(PlayerVibrator playerVibrator, Axles axles)
-        {
-            playerVibrator.axles = axles;
+            brakeEffects.RightBrakeLightTransform = brakelightsTransform.Find("BreakLight R");
+            brakeEffects.LeftBrakeLightTransform = brakelightsTransform.Find("BreakLight L");
         }
 
         private static void SetupCollider(GameObject carObj)
@@ -188,39 +108,62 @@ namespace CarsExtended
             touchCollider.ColliderScale = 1.6f;
         }
 
-        private static void SetupParticleManager(GameObject carObj)
+        private static void SetupDrivetrain(Drivetrain drivetrain, CarInfos carInfos)
+        {
+            drivetrain.transmissionForParticles = (TransmissionForParticles)Enum.Parse(
+                typeof(TransmissionForParticles),
+                carInfos.data.carStats.Transmission.ToString()
+            );
+            drivetrain.engineOrientation = carInfos.engineOrientation;
+        }
+
+        private static void SetupHeadlights(GameObject carObj, Transform lightsTransform)
+        {
+            HeadlightManager headlightManager = lightsTransform.Find("Headlights").gameObject.AddComponent<HeadlightManager>();
+            headlightManager.HeadlightPosition = headlightManager.transform.Find("Headlights");
+        }
+
+        private static void SetupParticleManager(GameObject carObj, Transform lightsTransform, Transform brakelightsTransform)
         {
             ParticleManager particleManager = carObj.transform.Find("PrefabSpawns").gameObject.AddComponent<ParticleManager>();
             Transform particlesRoot = particleManager.transform.Find("Particles");
-            Transform lightsRoot = particleManager.transform.Find("Lights");
 
             particleManager.AmbientEffectsPosition = particlesRoot.Find("Ambient Effects");
-            particleManager.Brakelight_Position_L = lightsRoot.Find("Brakelights/BrakeLight L");
-            particleManager.Brakelight_Position_R = lightsRoot.Find("Brakelights/BrakeLight R");
+            particleManager.Brakelight_Position_L = brakelightsTransform.Find("BrakeLight L");
+            particleManager.Brakelight_Position_R = brakelightsTransform.Find("BrakeLight R");
             particleManager.DustPosition = particlesRoot.Find("Dust");
             particleManager.EngineSteamPosition = particlesRoot.Find("EngineSteam");
-            particleManager.ExhaustBackfire_Position_1 = lightsRoot.Find("Backfire/Backfire L");
-            particleManager.ExhaustBackfire_Position_2 = lightsRoot.Find("Backfire/Backfire R");
+            particleManager.ExhaustBackfire_Position_1 = lightsTransform.Find("Backfire/Backfire L");
+            particleManager.ExhaustBackfire_Position_2 = lightsTransform.Find("Backfire/Backfire R");
             particleManager.Exhaust1Position = particlesRoot.Find("Exhaust1");
             particleManager.Exhaust2Position = particlesRoot.Find("Exhaust2");
         }
 
-        private static void SetupHeadlights(GameObject carObj)
+        private static void SetupPlayerVibrator(PlayerVibrator playerVibrator, Axles axles) => playerVibrator.axles = axles;
+
+        private static void SetupRenderers(GameObject carObj, CarInfos carInfos)
         {
-            HeadlightManager headlightManager = carObj.transform.Find("PrefabSpawns/Lights/Headlights").gameObject.AddComponent<HeadlightManager>();
-            headlightManager.HeadlightPosition = headlightManager.transform.Find("Headlights");
+            // TODO : How can I setup all the renderers ?
+            // TODO : How does the game load liveries ?
         }
 
-        private static void SetupShadow(GameObject carObj, CarInfos carInfos)
+        // force setup rigidbody because I don't trust humans
+        private static void SetupRigidbody(Rigidbody rigidbody)
         {
-            GameObject sourcePrefab = Resources.Load<GameObject>(
-                "Prefabs/Cars/" + carInfos.soundSourcePrefabName + "/" + carInfos.soundSourcePrefabName
-            );
+            rigidbody.drag = 0;
+            rigidbody.angularDrag = 0;
+            rigidbody.useGravity = true;
+            rigidbody.isKinematic = false;
+            rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+            rigidbody.constraints = RigidbodyConstraints.None;
+        }
 
-            Transform colliderTransform = carObj.transform.Find("Collider");
+        private static void SetupShadow(GameObject carObj, GameObject sourceCarPrefab)
+        {
             Transform shadowTransform = carObj.transform.Find("Shadow");
             Renderer renderer = shadowTransform.GetComponent<Renderer>();
-            renderer.sharedMaterial = sourcePrefab.transform.Find("Shadow").GetComponent<Renderer>().sharedMaterial;
+            renderer.sharedMaterial = sourceCarPrefab.transform.Find("Shadow").GetComponent<Renderer>().sharedMaterial;
 
             Vector3 groundPos = Vector3.zero;
             Wheel[] wheels = carObj.transform.GetComponentsInChildren<Wheel>();
@@ -235,12 +178,65 @@ namespace CarsExtended
                 .GetComponent<Collider>().bounds.size + new Vector3(1, 0, 1) * SHADOW_EXPANSION;
         }
 
-        private static void SetupRenderers(GameObject carObj, CarInfos carInfos)
+        private static void SetupSoundController(SoundController soundController, SoundController sourceSoundController)
         {
-            // TODO : How can I setup all the renderers ?
-            // TODO : How does the game load liveries ?
+            soundController.antiLagEnabled = sourceSoundController.antiLagEnabled;
+            soundController.playExhaustParticles = sourceSoundController.playExhaustParticles;
+            soundController.flatTireVolume = sourceSoundController.flatTireVolume;
+            soundController.flatTire = sourceSoundController.flatTire;
+            soundController.waterSplashNoise = sourceSoundController.waterSplashNoise;
+            soundController.rollingNoiseOffroad = sourceSoundController.rollingNoiseOffroad;
+            soundController.rollingNoiseSand = sourceSoundController.rollingNoiseSand;
+            soundController.rollingNoiseGrass = sourceSoundController.rollingNoiseGrass;
+            soundController.windVolume = sourceSoundController.windVolume;
+            soundController.wind = sourceSoundController.wind;
+            soundController.antilag1 = sourceSoundController.antilag1;
+            soundController.backfire3 = sourceSoundController.backfire3;
+            soundController.backfire2 = sourceSoundController.backfire2;
+            soundController.backfire1 = sourceSoundController.backfire1;
+            soundController.turboWasteGate3 = sourceSoundController.turboWasteGate3;
+            soundController.turboWasteGate2 = sourceSoundController.turboWasteGate2;
+            soundController.turboWasteGate1 = sourceSoundController.turboWasteGate1;
+            soundController.shiftTriggerVolume = sourceSoundController.shiftTriggerVolume;
+            soundController.shiftTrigger = sourceSoundController.shiftTrigger;
+            soundController.scrapeNoiseVolume = sourceSoundController.scrapeNoiseVolume;
+            soundController.scrapeNoise = sourceSoundController.scrapeNoise;
+            soundController.crashLowVolume = sourceSoundController.crashLowVolume;
+            soundController.crashLowSpeed = sourceSoundController.crashLowSpeed;
+            soundController.crashHighVolume = sourceSoundController.crashHighVolume;
+            soundController.crashHiSpeed = sourceSoundController.crashHiSpeed;
+            soundController.skidPitchFactor = sourceSoundController.skidPitchFactor;
+            soundController.gravelSkid = sourceSoundController.gravelSkid;
+            soundController.tarmacWetSkid = sourceSoundController.tarmacWetSkid;
+            soundController.tarmacDrySkid = sourceSoundController.tarmacDrySkid;
+            soundController.brakeNoiseVolume = sourceSoundController.brakeNoiseVolume;
+            soundController.brakeNoise = sourceSoundController.brakeNoise;
+            soundController.transmissionSourcePitch = sourceSoundController.transmissionSourcePitch;
+            soundController.transmissionVolumeReverse = sourceSoundController.transmissionVolumeReverse;
+            soundController.transmissionVolume = sourceSoundController.transmissionVolume;
+            soundController.transmission = sourceSoundController.transmission;
+            soundController.startEnginePitch = sourceSoundController.startEnginePitch;
+            soundController.startEngineVolume = sourceSoundController.startEngineVolume;
+            soundController.startEngine = sourceSoundController.startEngine;
+            soundController.engineNoThrottlePitchFactor = sourceSoundController.engineNoThrottlePitchFactor;
+            soundController.engineNoThrottleVolume = sourceSoundController.engineNoThrottleVolume;
+            soundController.engineNoThrottle = sourceSoundController.engineNoThrottle;
+            soundController.engineThrottlePitchFactor = sourceSoundController.engineThrottlePitchFactor;
+            soundController.engineThrottleVolume = sourceSoundController.engineThrottleVolume;
+            soundController.engineThrottle = sourceSoundController.engineThrottle;
+        }
+
+        private static void SetupWings(GameObject carObj)
+        {
+            carObj.transform.Find("Wing_Front").gameObject.AddComponent<Wing>().WingPosition = Wing.WING_POSITION.FRONT;
+            carObj.transform.Find("Wing_Back").gameObject.AddComponent<Wing>().WingPosition = Wing.WING_POSITION.FRONT;
         }
 
         // Custom/CarShader1 <= shader used for cars
+        // Main texture ?
+        // Normal ?
+        // Colors ?
+        // TODO : Map materials from CarShader
+        // TODO : Map which shaders are used where on the car
     }
 }
